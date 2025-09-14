0b68e3003b4bd8a7ac05678a47fabe19927b3201
ricardo garcia <ricardogarci4@gmail.com>
2025-09-14 19:05:32 -0300

    clase 04 - express + get/post + middleware

---
+++ clase-04/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  console.log("Hola", req.query)
+    res.send("Hola sr " + req.query.nombre);
+});
+
+app.post('/', (req, res) => {
+  console.log("Hola", req.body)
+  res.send("Hola" );
+})
+
+app.listen(2025, () => {
+  console.log('Servidor corriendo en el puerto 2025');
+});
