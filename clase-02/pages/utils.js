775ccae15cfcce2636a85ddb2f5e58c752a99a31
ricardo garcia <ricardogarci4@gmail.com>
2025-09-14 00:51:47 -0300

    clase 03 - modulalizacion

---
+++ clase-02/pages/utils.js
@@ -0,0 +1,65 @@
+function createPage(titulo, contenido) {
+    let html = ""
+    html += '<!doctype html><html lang="es"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0">'
+    html += '<title>'+titulo+'</title>'
+    html += `
+    <style>     
+      body {
+        font-family: Arial, sans-serif;
+        background: linear-gradient(135deg, #2c3e50, #34495e);
+        color: #fff;              
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;      
+        text-align: center;
+      }
+
+      h1 {
+        font-size: 2.5em;
+        margin-bottom: 20px;
+        color: #f39c12;
+      }
+
+      table {
+        border-collapse: collapse;
+        width: 50%;
+        margin: 20px auto;
+        background: #34495e;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);
+      }
+
+      th,
+      td {
+        padding: 12px;
+        border: 1px solid #2c3e50;
+        text-align: center;
+        color: #ecf0f1;
+      }
+
+      th {
+        background: #f39c12;
+        color: #2c3e50;
+        font-weight: bold;
+      }     
+    </style>
+  </head>
+  <body>
+  <h1>Mi espectacular pagina web!</h1>
+    `
+    html += contenido
+    html += '</body></html>'
+    return html
+}
+
+function createProductList(productos){
+    let html = "<table>"
+    html += "<tr><th>ID</th><th>Nombre</th><th>Precio</th></tr>"
+    productos.forEach(cafe => {
+        html += `<tr><td>${cafe.id}</td><td>${cafe.nombre}</td><td>${cafe.precio}</td></tr>`
+    })
+    html += "</table>"
+    return html
+}
+
+module.exports = {createPage, createProductList}
--- clase-02/server.js
+++ clase-02/server.js
@@ -1,92 +1,29 @@
 const http = require("http")
 
-const productos = [
-    {id: 1, nombre: "Café Expreso", precio: 200},
-    {id: 2, nombre: "Café Americano", precio: 250},
-    {id: 3, nombre: "Café Cortado", precio: 200},
-    {id: 4, nombre: "Café Doble", precio: 250},
-    {id: 5, nombre: "Café Lagrima", precio: 200}
-];
+const cafes = require("./data/productos.js");
+const pages = require( "./pages/utils.js" )
 
 const server = http.createServer(function (request, response) {
-    response.write(`<!doctype html>
-<html lang="es">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>     
-      body {
-        font-family: Arial, sans-serif;
-        background: linear-gradient(135deg, #2c3e50, #34495e);
-        color: #fff;              
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;      
-        text-align: center;
-      }
 
-      h1 {
-        font-size: 2.5em;
-        margin-bottom: 20px;
-        color: #f39c12;
-      }
-
-      table {
-        border-collapse: collapse;
-        width: 50%;
-        margin: 20px auto;
-        background: #34495e;
-        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);
-      }
-
-      th,
-      td {
-        padding: 12px;
-        border: 1px solid #2c3e50;
-        text-align: center;
-        color: #ecf0f1;
-      }
-
-      th {
-        background: #f39c12;
-        color: #2c3e50;
-        font-weight: bold;
-      }     
-    </style>
-  </head>
-  <body>`);
-
-    console.log("Route que me envio el cliente: ", request.url)
-    response.write("<h1>Mi espectacular pagina web!</h1>")
     switch (request.url) {
         case "/":
-            response.write("Ricardo Garcia")
+            response.write(pages.createPage("home","Ricardo Garcia"))
             break
         case "/materia":
-            response.write("Aplicaciones Hibridas")
+            response.write(pages.createPage("materia","Aplicaciones Hibridas"))
             break
         case "/profesor":
-            response.write("Victor Villafañe")
+            response.write(pages.createPage("profesor","Victor Villafañe"))
             break
-        case "/productos":
-            response.write("<table>")
-            response.write("<tr><th>ID</th><th>Nombre</th><th>Precio</th></tr>")
-            productos.forEach(producto => {
-                response.write(`<tr><td>${producto.id}</td><td>${producto.nombre}</td><td>${producto.precio}</td></tr>`)
-            })
-            response.write("</table>")
+        case "/cafes":
+         response.write(pages.createPage("Cafes", pages.createProductList(cafes)))
             break
         default:
-            response.write("No se encontro la ruta")
+            response.write(pages.createPage("404","No se encontro la ruta"))
             break
     }
+    response.end()
 
-    response.end(`
-            </body>
-        </html>
-    `);
 })
 
 server.listen(2025, () => {
