e21e9164f66579d9e308a80b1f70ed7fc8adf6bb
ricardo garcia <ricardogarci4@gmail.com>
2025-09-14 19:01:10 -0300

    clase 03 - fin

--- clase/server.js
+++ clase/server.js
@@ -5,19 +5,31 @@ import {readFile} from "fs"
 
 const server = http.createServer(function (request, response) {
 
-
     switch (request.url) {
         case "/":
             response.write(createPage("home", "Ricardo Garcia"))
+            response.end()
+
             break
         case "/materia":
             response.write(createPage("materia", "Aplicaciones Hibridas"))
+            response.end()
+
             break
         case "/profesor":
             response.write(createPage("profesor", "Victor Villafañe"))
+            response.end()
+
             break
         case "/cafes":
             response.write(createPage("Cafes", createProductList(cafes)))
+            break
+        case "/index.html":
+            readFile("public/index.html", (err, data) => {
+                if (err) console.error("No se encontro el archivo")
+                response.write(data)
+                response.end()
+            })
             break
         case "/contacto.html":
             readFile("public/contacto.html", (err, data) => {
@@ -33,6 +45,13 @@ const server = http.createServer(function (request, response) {
                 response.write(data)
                 response.end()
 
+            })
+            break
+        case "/img/prueba.png":
+            readFile("public/img/prueba.png", (err, data) => {
+                if (err) console.error("No se encontro el archivo")
+                response.write(data)
+                response.end()
             })
             break
         default:
