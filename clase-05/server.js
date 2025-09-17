d11cbe9f1b3a1de7a44151742b5d34dcee01b5d7
ricardo garcia <ricardogarci4@gmail.com>
2025-09-17 17:57:47 -0300

    clase 05 - json + promesas asincronicas con get

--- clase-05/server.js
+++ clase-05/server.js
@@ -1,5 +1,21 @@
 import express from "express";
+import {readFile} from "node:fs/promises";
+
 const app = express();
 
+app.get("/", async (req, res) => {
+    readFile("data/productos.json", "utf-8")
+        .then(data => JSON.parse(data))
+        .catch(err => [])
+        .then((productos) => {
+            let html = "<ul>"
+            productos.forEach(producto => {
+                html += `<li>${producto.modelo}</li>`
+            })
+            html += "</ul>"
+            res.send(html)
+        }
+)
+})
 
 app.listen(2025, () => console.log("funcionando"));
