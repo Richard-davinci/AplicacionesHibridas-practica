786fd97ff0e868b126e70f976d8baf4371dd2ec5
ricardo garcia <ricardogarci4@gmail.com>
2025-09-18 00:08:27 -0300

    clase 05 - creacion de controller, viewa, routes y servirces

--- clase-05/server.js
+++ clase-05/server.js
@@ -1,64 +1,7 @@
-import express from "express"
-import {readFile} from "node:fs/promises"
-import {createPage} from "./pages/utils.js"
+import express from "express";
+import ProductosRoute from "./routes/productos.routes.js"
+const app = express();
 
-const app = express()
+app.use(ProductosRoute)
 
-async function getProductos() {
-  return readFile("./data/productos.json", "utf-8")
-    .then((data) => JSON.parse(data))
-    .catch((err) => [])
-}
-
-function createProductosListPage(productos) {
-  let html = "<ul>"
-  productos.forEach((producto) => {
-    html += `<li>${producto.modelo} <a href="${producto.id}">Ver</a></li>`;
-  });
-  html += "</ul>"
-  return html
-}
-
-async function getProductoById(id) {
-  return getProductos()
-    .then((productos) => {
-      let producto
-      for (let i = 0; i < productos.length; i++) {
-        if (productos[i].id === id) {
-          producto = productos[i]
-        }
-      }
-      return producto
-    })
-}
-
-app.get("/", async (req, res) => {
-  getProductos()
-    .then((productos) => {
-      res.send(createPage("Listado de productos", createProductosListPage(productos)));
-    });
-});
-//lista de detalles
-app.get("/:id", (req, res) => {
-  const id = Number(req.params.id)
-  getProductoById(id)
-    .then(producto => {
-      let html = ""
-      if (producto) {
-        html += "<ul>"
-        html += `<li>Marca: ${producto.marca}</li>`
-        html += `<li>Modelo: ${producto.modelo}</li>`
-        html += `<li>Precio: ${producto.precio}</li>`
-        html += "</ul>"
-        html += "<a href='/'>Volver</a>"
-        html += "</ul>"
-        res.send(createPage("Detalles del producto", html))
-      } else {
-        html += "<h2>Producto no encontrado</h2>"
-        html += "<a href='/'>Volver</a>"
-        res.send(createPage("404", html))
-
-      }
-    })
-})
 app.listen(2025, () => console.log("funcionando"));
--- clase-05/views/productos.views.js
+++ clase-05/views/productos.views.js
@@ -1,8 +1,10 @@
 export function createPage(titulo, contenido) {
-    let html = ""
-    html += '<!doctype html><html lang="es"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0">'
-    html += '<title>'+titulo+'</title>'
-    html += `
+  let html = "";
+  html +=
+    '<!DOCTYPE html><html lang="es"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0">';
+  html += "<title>" + titulo + "</title>";
+  html += "</head>";
+  html += `
     <style>     
       body {
         font-family: Arial, sans-serif;
@@ -21,27 +23,7 @@ export function createPage(titulo, contenido) {
         color: #f39c12;
       }
 
-      table {
-        border-collapse: collapse;
-        width: 50%;
-        margin: 20px auto;
-        background: #34495e;
-        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);
-      }
-
-      th,
-      td {
-        padding: 12px;
-        border: 1px solid #2c3e50;
-        text-align: center;
-        color: #ecf0f1;
-      }
-
-      th {
-        background: #f39c12;
-        color: #2c3e50;
-        font-weight: bold;
-      }     
+      
       /* Estilo de la lista de detalles */
         ul {
             list-style: none;
@@ -89,26 +71,38 @@ export function createPage(titulo, contenido) {
             background: #1d3557;
             transform: scale(0.98);
         }
+    </style>`
+  html += `<body><h1>${titulo}</h1>`;
+  html += contenido;
+  html += "</body></html>";
 
-    </style>
-  </head>
-  <body>
-  <h1>${titulo}</h1>
-    `
-    html += contenido
-    html += '</body></html>'
-    return html
+  return html;
 }
 
-export function createProductList(productos){
-    let html = "<table>"
-    html += "<tr><th>ID</th><th>Nombre</th><th>Precio</th></tr>"
-    productos.forEach(cafe => {
-        html += `<tr><td>${cafe.id}</td><td>${cafe.nombre}</td><td>${cafe.precio}</td></tr>`
-    })
-    html += "</table>"
-    return html
+export function createProductosListPage(productos) {
+  let html = "<ul>";
+  productos.forEach((producto) => {
+    html += `<li>${producto.modelo} <a href="/productos/${producto.id}">Ver</a></li>`;
+  });
+  html += "</ul>";
+
+  return createPage("Productos", html);
 }
 
-/*
-export {createPage, createProductList}*/
+export function createDetailPage(producto) {
+  let html = "";
+  html += "<ul>";
+  html += `<li>Marca: ${producto.marca}</li>`;
+  html += `<li>Modelo: ${producto.modelo}</li>`;
+  html += `<li>Precio: ${producto.precio}</li>`;
+  html += "</ul>";
+  html += `<a href="/productos">Volver</a>`;
+  return createPage(producto.modelo, html);
+}
+
+export function errorPage() {
+  let html = "";
+  html += "<h2>No se encontro el producto buscado</h2>";
+  html += `<a href="/productos">Volver</a>`;
+  return createPage("404", html);
+}
